83493b6ff22dfdbe4212f73a4c8b6243c6587692
Vansh Rathore <73779153+VanshRathore@users.noreply.github.com>
2024-09-20 23:47:00 +0530

    added Game Over Logic

--- script.js
+++ script.js
@@ -30,16 +30,68 @@ document.addEventListener("DOMContentLoaded" , () => {
             const element = drawDiv(snakeCell.x, snakeCell.y, 'sanke');
             gameArena.appendChild(element);
         })
-
+ 
         const foodElement = drawDiv(food.x, food.y, 'food');
         gameArena.appendChild(foodElement);
     }
 
+    function moveFood(){
+        let newX, newY;
+        do{
+            newX = Math.floor(Math.random() * ((arenaSize - cellSize)/cellSize)*cellSize);
+            newY = Math.floor(Math.random() * ((arenaSize - cellSize)/cellSize)*cellSize);
+        } while(snake.some(snakeCell => snakeCell.x == newX && snakeCell.y == newY));
+
+        food = {x:newX, y: newY};
+    }
+
+    function updateSnake(){
+        console.log("Update snake called")
+        // calculate new coordinate the sanke head will go
+        const newHead = {x: snake[0].x+dx, y: snake[0].y + dy}
+        snake.unshift(newHead); //add new head
+        if(newHead.x == food.x && newHead.y == food.y){
+            // collision
+            //dont pop the element
+            score += 5;
+            // move food
+            moveFood();
+
+        } else{
+            snake.pop(); //remove last element
+        }
+    }
+
+    function isGameOver(){
+        // check sanke body hit
+        for(i = 1; i < snake.length; i++){
+            if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) return true;
+        }
+
+        // check wall collision
+        const isHittingLeftWall = snake[0].x < 0;
+        const isHittingTopWall = snake[0].y < 0;
+        const isHittingRightWall = snake[0].x >= arenaSize;
+        const isHittingDownWall = snake[0].y >= arenaSize;
+
+        return isHittingDownWall || isHittingLeftWall || isHittingRightWall || isHittingTopWall ; //game over
+
+    }
+
     function gameLoop(){
         setInterval(()=>{
+            if(!gameStarted) return;
+
+            if(isGameOver()){
+                gameStarted = false;
+                alert(`Game Over, Score = ${score}`);
+                window.location.reload();
+                return;
+            }
+            updateSnake();
             drawScoreBoard();
             drawFoodAndSnake();
-        },1000);
+        },500);
     }
 
     function runGame(){
